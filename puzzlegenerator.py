6fe877453ec46655fdfb0350d776f09803a0f190
sourav <sourav.tripathy@gmail.com>
2020-05-25 02:04:48 +0530

    Generalized row/col selection

--- puzzlegenerator.py
+++ puzzlegenerator.py
@@ -5,15 +5,17 @@ words = ["Java", "Python", "GoLang", "NodeJs", "DotNet", "Pascal"]
 
 longestStr = max(words, key=len)
 
-if (len(longestStr)) > 12:
+gridLen = 12
+
+if (len(longestStr)) > gridLen:
     print("Error")
 
-final_array = np.full([12, 12], '-')
+final_array = np.full([gridLen, gridLen], '-')
 
 for word in words:
+    word = word.upper()
 
-    row = random.randint(1, 4)
-    col = random.randint(1, 4)
+    wordLen = len(word)
 
     is_reverse = random.randint(0, 1)
 
@@ -21,6 +23,20 @@ for word in words:
         word = word[::-1]
 
     direction = random.randint(1, 3)
+
+    if direction == 1:
+        row = random.randint(1, (gridLen - wordLen))
+        col = random.randint(1, gridLen)
+    elif direction == 2:
+        row = random.randint(1, gridLen)
+        col = random.randint(1, (gridLen - wordLen))
+    else:
+        row = random.randint(1, (gridLen - wordLen))
+        col = random.randint(1, (gridLen - wordLen))
+
+    ## TODO : Overlapping row and columns
+    ## TODO : Overlapping cell values
+    ## TODO : Fix index logic
 
     for c in word:
         if direction == 1:
@@ -32,4 +48,5 @@ for word in words:
             col = col + 1
         final_array[row][col] = c
 
+## TODO : Fill unfiiled cells with random charaters
 print(final_array)
