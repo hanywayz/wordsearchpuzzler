d4d9f2594d31ca17ed73c6b1decb3359f6f50369
sourav <sourav.tripathy@gmail.com>
2020-05-27 00:36:01 +0530

    First Release Candidate

--- simplepuzzlegenerator.py
+++ simplepuzzlegenerator.py
@@ -9,94 +9,94 @@ words = ["KARNATAKA", "DELHI", "Kerala", "Manipur", "Mizoram", "Tripura", "Assam
 
 
 def fillWordGrid():
-    rowIndexList = generateUniqueIndex()
-    colIndexList = generateUniqueIndex()
-
     filledInWords = list()
 
     for word in words:
+        word = word.upper()
         word = get_if_reversed(word)
         wordLen = len(word)
 
-        # orientations = ['HORIZONTAL', 'VERTICAL', 'DIAGONAL']
         orientations = ['HORIZONTAL', 'VERTICAL']
 
         orientation = random.choice(orientations)
-        if orientation == 'HORIZONTAL':
-            row = rowIndexList.pop(0)
-            col = random.randint(0, (gridLen - wordLen - 1))
-        elif orientation == 'VERTICAL':
-            row = random.randint(0, (gridLen - wordLen - 1))
-            col = colIndexList.pop(0)
-        else:
-            # TODO : Fix based on max length
-            row = rowIndexList.pop(0)
-            col = colIndexList.pop(0)
+        col, row = getRowCol(orientation, wordLen)
+        isOverlapping = True
+        while isOverlapping:
+            isOverlapping = checkIfOverlapping(word, row, col, orientation, filledInWords)
+            if isOverlapping:
+                col, row = getRowCol(orientation, wordLen)
 
-        print("word:", word, "row:", row, "col:", col, "orientation:", orientation)
+        filledInWords.append([word, orientation, row, col])
 
-        isOverlapping = checkIfOverlapping(final_array, word, row, col, orientation)
+        # print("word:", word, "row:", row, "col:", col, "orientation:", orientation)
 
-        print("isOverlapping", isOverlapping, "word:", word)
+    print(filledInWords)
 
-        if isOverlapping:
-            # if orientation == 'HORIZONTAL':
-            #     col =
-            # elif orientation == 'VERTICAL':
-            #     row =
-            # else:
-            #
-            conflictingWords = filledInWords
-
-            row, col = resolveConflicts(word, row, col, orientation,conflictingWords)
-        
-        for c in word:
+    for eachWord in filledInWords:
+        row = eachWord[2]
+        col = eachWord[3]
+        orientation = eachWord[1]
+        for c in eachWord[0]:
             final_array[row][col] = c
             if orientation == 'HORIZONTAL':
                 col = col + 1
             elif orientation == 'VERTICAL':
                 row = row + 1
-            else:
-                row = row + 1
-                col = col + 1
-
-        filledInWords.append(word)
-
     print(final_array)
+
+
+def getRowCol(orientation, wordLen):
+    if orientation == 'HORIZONTAL':
+        row = random.randint(0, 11)
+        col = random.randint(0, (gridLen - wordLen - 1))
+    elif orientation == 'VERTICAL':
+        row = random.randint(0, (gridLen - wordLen - 1))
+        col = random.randint(0, 11)
+    return col, row
+
+
+def checkIfOverlapping(word, row, col, orientation, filledInWords):
+    overlapping = False
+    wordCells = getCells(word, row, col, orientation)
+
+    # print("CHECK OVERLAP word:", word)
+
+    for filledInWord in filledInWords:
+        tempWordCells = getCells(filledInWord[0], filledInWord[2], filledInWord[3], filledInWord[1])
+        for refCell in tempWordCells:
+            for wordCell in wordCells:
+                if refCell[1] == wordCell[1] and refCell[2] == wordCell[2]:
+                    overlapping = True
+                    # print("overlapping!!! cell :", wordCell[1], wordCell[2], "word :", word, "row:", row, "col:", col,
+                    #       "orientation:", orientation,
+                    #       "filledInWord", filledInWord)
+                    # print("wordCells", wordCells, "tempWordCells", tempWordCells)
+                    return True
+        # print("CheckIfOverlapping word:", word, "filledInWord[0]:",filledInWord[0], "overlapping:",overlapping)
+    return overlapping
+
+
+def getCells(word, row, col, orientation):
+    cells = list()
+    for c in word:
+        cells.append([c, row, col])
+        if orientation == 'HORIZONTAL':
+            col = col + 1
+        elif orientation == 'VERTICAL':
+            row = row + 1
+    return cells
 
 
 def resolveConflicts(word, row, col, orientation, conflictingWords):
     return row, col
 
 
-def generateUniqueIndex():
-    randomValues = list(range(0, 11))
-    random.shuffle(randomValues)
-    return randomValues
-
-
-def checkIfOverlapping(tempData, word, row, col, orientation):
-    overlapping = False
-    for c in word:
-        if (tempData[row][col]) != '-':
-            overlapping = True;
-            break
-            
-        if orientation == 'HORIZONTAL':
-            col = col + 1
-        elif orientation == 'VERTICAL':
-            row = row + 1
-        else:
-            row = row + 1
-            col = col + 1
-    return overlapping
-
 def get_if_reversed(word):
-    word = word.upper()
     is_reverse = random.randint(0, 1)
     if is_reverse:
         word = word[::-1]
     return word
+
 
 class WordSearchGenretor:
     if __name__ == "__main__":
